b1307c4c8b877a30d505182c4260091a2b1dbc6f
Radiantly <44368997+radiantly@users.noreply.github.com>
2020-03-09 11:47:21 +0530

    Import bad-words lib

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+const Filter = require('bad-words');
 
 try {
   console.log("Hello World!");
-  if (github.context.eventName === 'issue') {
-    core.info(`The context contains: ${JSON.stringify(github.context)}`);
+  let currentContext = github.context;
+  if (currentContext.eventName === 'issue') {
+    let filter = new Filter();
+    let title = currentContext.issue.title;
+    let body = currentContext.issue.body;
+    let cleanTitle = filter.clean(title);
+    let cleanBody = filter.clean(body);
+    if(cleanTitle === title) console.log("Title is clean!");
+    if(cleanBody == body) console.log("Title is clean!");
   }
 } catch (error) {
   core.setFailed(error.message);
